72cfb6b6f9c2482bced1e5966cd28a75a7063ea6
xyzuan <xyzuannihboss@gmail.com>
2022-06-11 01:20:54 +0700

    kebeletEgg: Initial push our secret Simple EasterEgg

---
+++ app/src/main/java/id/xyzprjkt/xd/kebeletegg/kebeletMain.java
@@ -0,0 +1,13 @@
+package id.xyzprjkt.xd.kebeletegg;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kebeletMain extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xd_main);
+    }
+}
